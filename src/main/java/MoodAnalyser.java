f3f79ddd8a5f4ac28f9a08ec21f51d0ae366a4c8
Amet-Vikram <amet1197@gmail.com>
2022-04-11 00:07:46 +0530

    Refactored to take message in constructor.

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,9 +1,17 @@
 
 
 public class MoodAnalyser {
+    private String msg;
 
-    public String analyseMood(String message) {
-        if(message.contains("sad")){
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String msg){
+        this.msg = msg;
+    }
+
+    public String analyseMood() {
+        if(msg.contains("sad")){
             return "Sad";
         }
         return "Happy";
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -2,17 +2,18 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MoodAnalyserTest {
-    MoodAnalyser moodAnalyser = new MoodAnalyser();
-
+    
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
-        String mood = moodAnalyser.analyseMood("I am in sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("Sad", mood);
     }
 
     @Test
     public void givenMessage_WhenHappy_ShouldReturnHappy(){
-        String mood = moodAnalyser.analyseMood("I am in any mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("Happy",mood);
     }
 }
